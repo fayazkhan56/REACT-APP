49206e48b5881a5b6e67c558641ac193f680e752
fayazkhan56 <68395705+fayazkhan56@users.noreply.github.com>
2020-08-15 04:24:00 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import './hello.css';
 
-function App({name,age}) {
-return <div className="myround">hello world from {name} with age of {age} </div>
+function App({name}) {
+return <div className="myround">
+    hello world from <strong>{name}</strong> </div>
 
 }
 
