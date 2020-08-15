99479a18c4a206944b8b2f454218eea1526f2f35
fayazkhan56 <68395705+fayazkhan56@users.noreply.github.com>
2020-08-15 04:19:00 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './hello.css';
 
 function App({name,age}) {
-return <div>hello world from {name} with age of {age} </div>
+return <div className="myround">hello world from {name} with age of {age} </div>
 
 }
 
