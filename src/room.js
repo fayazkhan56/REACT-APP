c969822678f4653921ee6ae87ceaa2e2f2f25886
fayazkhan56 <68395705+fayazkhan56@users.noreply.github.com>
2020-08-16 04:59:24 -0700

    update

--- src/room.js
+++ src/room.js
@@ -2,22 +2,20 @@ import React, { useState } from 'react';
 import './room.css';
 function Room() {
 
-  let (islit, setlit)=useState(false);
-  let (age  , setage)=useState(24);
+  let [islit, setlit]=useState(false);
+  let [age, setage]=useState(24);
 
   
     return (
-      <div className={'room ${islit? "lit" : "dark"}'}>
-         This room is (islit? lit : dark);
+     <div className={'room ${islight? "lit" : "dark"}'}>
+         This room is {islit? "lit" : "dark"};
        <br/>
-        age : (age)
+        age : {age};
        <br/>
       
-        <button onClick={()=> setlit(!setlit)} >light button</button>
+        <button onClick={()=> setlit(!islit)} >light button</button>
 
-        <button>
-          onClick{ () => setage(++age)} increase age 
-        </button>
+        <button onClick ={()=> setage(++age)}> increase age</button>
           </div>
     );
   }
